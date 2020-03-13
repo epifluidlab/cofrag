fbe5d14b483b5a358735cba908be18e7a42f0fc5
Haizi Zheng <haizi.zheng@cchmc.org>
2020-03-13 11:57:18 -0400

    New features
    * A function to plot Hi-C matrix

---
+++ utils.R
@@ -0,0 +1,64 @@
+library(gplots)
+
+default_tick_func <- function(ticks)
+  sapply(round(ticks / 1000), function(v)
+    sprintf("%s kb", format(v, big.mark = ",")))
+
+plot_hic_matrix <-
+  function(m,
+           start,
+           end,
+           tick_func = default_tick_func,
+           title = NULL,
+           ...) {
+    my.image <- function(figData,  zlim, col, na.color = 'gray', ...)
+    {
+      newz.na <-
+        zlim[2] + (zlim[2] - zlim[1]) / length(col) # new z for NA
+      figData[which(is.na(figData))] <-
+        newz.na # we affect newz.outside
+      zlim[2] <-
+        zlim[2] + (zlim[2] - zlim[1]) / length(col) # we finally extend the z limits to include the two new values
+      col <-
+        c(col, na.color) # we construct the new color range by including: na.color and outside.color
+      image(figData,  zlim = zlim, col = col, ...) # we finally call image(...)
+    }
+    
+    cp <- colorpanel(64, "#FB040A", "#E5C2C3", "white")
+    min_val <- min(as.vector(dm[!is.na(dm)]))
+    max_val <- max(as.vector(dm[!is.na(dm)]))
+    zlim <- c(min_val, max_val)
+    my.image(
+      m[, nrow(m):1],
+      zlim,
+      cp,
+      na.color = "gray",
+      useRaster = TRUE,
+      axes = FALSE,
+      ...
+    )
+    
+    ticks <- tick_func(seq(start, end, length.out = 6))
+    axis(
+      2,
+      at = seq(0, 1, length.out = 6),
+      labels = rev(ticks),
+      srt = 45,
+      tick = TRUE
+    )
+    axis(
+      3,
+      at = seq(0, 1, length.out = 6),
+      labels = ticks,
+      srt = 45,
+      tick = TRUE
+    )
+    if (is.null(title))
+      main_title <-
+      sprintf(
+        "Hi-C heatmap for %s-%s",
+        format(start, digits = 15, big.mark = ","),
+        format(end, digits = 15, big.mark = ",")
+      )
+    title(main_title, line = 2.5)
+  }
