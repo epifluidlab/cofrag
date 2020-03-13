793dc89874860f985e21c64a2c7fc5ae782319a5
Haizi Zheng <haizi.zheng@cchmc.org>
2020-03-13 10:42:06 -0400

    New features:
    * When the genomic range is not multiples of the bin size, or when the
      block size is not multiples of the bin size, the program will try
      making paddings automatically.
    * Better-looking logging (by inserting "," separators in large numbers)
    
    Fix bugs:
    
    * Fails when no the BAM file contains no qualified reads.

--- calc_distance.R
+++ calc_distance.R
@@ -4,8 +4,6 @@
 # may be used to infer information about chromosome 3D conformation. To achieve this, we start
 # from calculating the statistical distance between any two binned segments
 
-
-
 library(logging)
 basicConfig()
 
@@ -49,7 +47,7 @@ calc_bfp <-
              nrow = 2,
              ncol = ncol(bin_layout))
     
-    for (idx in 1:length(aligned_reads$pos)) {
+    for (idx in seq(1, length(aligned_reads$pos), by = 1)) {
       # Scan the aligned reads (sorted bam) to calcuate the binning
       pos <- aligned_reads$pos[idx]
       if (pos < bin_layout[1, bin_idx])
@@ -155,127 +153,164 @@ calc_distance_by_block <-
     dist_vec
   }
 
-calc_distance_helper <- function(aligned_reads, gr, bin_size, block_size, nthreads, opts) {
-  # Divide the genome of interest into blocks
-  gr_start <- start(gr)
-  gr_end <- end(gr)
-  block_num <- round(width(gr) / block_size)
-  
-  loginfo(sprintf(
-    "Region-of-interest has been divided into %d sub-regions",
-    block_num
-  ))
-  
-  # Example: c(IRanges(1, 100), IRanges(101, 200))
-  block_layout <- lapply(1:block_num, function(i) {
-    if (i < block_num)
-      rg <- IRanges(start = gr_start + (i - 1) * block_size,
-                    end = gr_start + i * block_size - 1)
-    else
-      rg <-
-        IRanges(start = gr_start + (i - 1) * block_size, end = gr_end)
+calc_distance_helper <-
+  function(aligned_reads,
+           gr,
+           bin_size,
+           block_size,
+           nthreads,
+           opts) {
+    # Divide the genome of interest into blocks
+    gr_start <- start(gr)
+    gr_end <- end(gr)
+    block_num <- round(width(gr) / block_size)
     
     loginfo(sprintf(
-      "Sub-region #%d: %s:%d-%d, width: %d",
-      i,
-      seqnames(gr),
-      start(rg),
-      end(rg),
-      width(rg)
+      "Region-of-interest has been divided into %d sub-regions",
+      block_num
     ))
-    rg
-  })
-  
-  # Calculate the binned fragmentation profile for the entire genomic range
-  loginfo("Calculating the binned fragmentation profile")
-  bfp <- calc_bfp(aligned_reads, gr, bin_size)
-  
-  # Calculate the distance matrixes block by block. Row first.
-  # dist_matrix_list <- list()
-  
-  block_pairs <- list()
-  block_idx <- 0
-  for (row_idx in 1:block_num) {
-    for (col_idx in row_idx:block_num) {
-      block_idx <- block_idx + 1
-      block_pairs[[block_idx]] <- c(row_idx, col_idx)
-    }
-  }
-  
-  loginfo("Calculating the distance matrix")
-  
-  gr_name = as.character(seqnames(gr))
-  cl <- makeCluster(nthreads)
-  registerDoParallel(cl)
-  dist_matrix_list <-
-    foreach (
-      pair = block_pairs,
-      # .combine = "c",
-      # .multicombine = TRUE,
-      .export = c(
-        "loginfo",
-        "logdebug",
-        "calc_distance_by_block",
-        "clip_bfp",
-        "ks_distance",
-        "start",
-        "end"
+    
+    # Example: c(IRanges(1, 100), IRanges(101, 200))
+    block_layout <- lapply(1:block_num, function(i) {
+      if (i < block_num)
+        rg <- IRanges(start = gr_start + (i - 1) * block_size,
+                      end = gr_start + i * block_size - 1)
+      else
+        rg <-
+          IRanges(start = gr_start + (i - 1) * block_size, end = gr_end)
+      
+      loginfo(
+        sprintf(
+          "Sub-region #%d: %s:%s-%s, width: %s",
+          i,
+          seqnames(gr),
+          format(start(rg), big.mark = ","),
+          format(end(rg), big.mark = ","),
+          format(width(rg), big.mark = ",")
+        )
       )
-    ) %dopar% {
+      rg
+    })
+    
+    # Calculate the binned fragmentation profile for the entire genomic range
+    loginfo("Calculating the binned fragmentation profile")
+    bfp <- calc_bfp(aligned_reads, gr, bin_size)
+    
+    # Calculate the distance matrixes block by block. Row first.
+    # dist_matrix_list <- list()
+    
+    block_pairs <- list()
+    block_idx <- 0
+    for (row_idx in 1:block_num) {
+      for (col_idx in row_idx:block_num) {
+        block_idx <- block_idx + 1
+        block_pairs[[block_idx]] <- c(row_idx, col_idx)
+      }
+    }
+    
+    loginfo("Calculating the distance matrix")
+    
+    gr_name = as.character(seqnames(gr))
+    cl <- makeCluster(nthreads)
+    registerDoParallel(cl)
+    dist_matrix_list <-
+      foreach (
+        pair = block_pairs,
+        # .combine = "c",
+        # .multicombine = TRUE,
+        .export = c(
+          "loginfo",
+          "logdebug",
+          "calc_distance_by_block",
+          "clip_bfp",
+          "ks_distance",
+          "start",
+          "end"
+        )
+      ) %dopar% {
+        row_idx <- pair[1]
+        col_idx <- pair[2]
+        
+        bfp_pair <-
+          list(
+            clip_bfp(
+              bfp,
+              start(block_layout[[row_idx]]),
+              end(block_layout[[row_idx]]),
+              gr_start,
+              bin_size
+            ),
+            clip_bfp(
+              bfp,
+              start(block_layout[[col_idx]]),
+              end(block_layout[[col_idx]]),
+              gr_start,
+              bin_size
+            )
+          )
+        
+        calc_distance_by_block(bfp_pair, function(a, b) {
+          ks_distance(a, b, opts$min_samples)
+        })
+      }
+    
+    stopCluster(cl)
+    
+    loginfo("Completed calculating the distances")
+    
+    # Conver to standard matrix representation
+    dm <-
+      matrix(rep(NA, (width(gr) / bin_size) ^ 2),
+             ncol = width(gr) / bin_size,
+             nrow = width(gr) / bin_size)
+    block_idx <- 0
+    for (pair in block_pairs) {
+      block_idx <- block_idx + 1
       row_idx <- pair[1]
       col_idx <- pair[2]
       
-      bfp_pair <-
-        list(
-          clip_bfp(
-            bfp,
-            start(block_layout[[row_idx]]),
-            end(block_layout[[row_idx]]),
-            gr_start,
-            bin_size
-          ),
-          clip_bfp(
-            bfp,
-            start(block_layout[[col_idx]]),
-            end(block_layout[[col_idx]]),
-            gr_start,
-            bin_size
-          )
-        )
+      row_start <-
+        (start(block_layout[[row_idx]]) - gr_start) / bin_size + 1
+      row_end <-
+        (end(block_layout[[row_idx]]) + 1 - gr_start) / bin_size
+      col_start <-
+        (start(block_layout[[col_idx]]) - gr_start) / bin_size + 1
+      col_end <-
+        (end(block_layout[[col_idx]]) + 1 - gr_start) / bin_size
       
-      calc_distance_by_block(bfp_pair, function(a, b) { ks_distance(a, b, opts$min_samples) })
+      dm[row_start:row_end, col_start:col_end] <-
+        dist_matrix_list[[block_idx]]
     }
-  
-  stopCluster(cl)
-  
-  loginfo("Completed calculating the distances")
-  
-  # Conver to standard matrix representation
-  dm <-
-    matrix(rep(NA, (width(gr) / bin_size) ^ 2),
-           ncol = width(gr) / bin_size,
-           nrow = width(gr) / bin_size)
-  block_idx <- 0
-  for (pair in block_pairs) {
-    block_idx <- block_idx + 1
-    row_idx <- pair[1]
-    col_idx <- pair[2]
-    
-    row_start <-
-      (start(block_layout[[row_idx]]) - gr_start) / bin_size + 1
-    row_end <-
-      (end(block_layout[[row_idx]]) + 1 - gr_start) / bin_size
-    col_start <-
-      (start(block_layout[[col_idx]]) - gr_start) / bin_size + 1
-    col_end <-
-      (end(block_layout[[col_idx]]) + 1 - gr_start) / bin_size
-    
-    dm[row_start:row_end, col_start:col_end] <-
-      dist_matrix_list[[block_idx]]
+    # Make the distance matrix symmetric
+    dm[lower.tri(dm)] <- 0
+    dm + t(dm)
   }
-  # Make the distance matrix symmetric
-  dm[lower.tri(dm)] <- 0
-  dm + t(dm)
+
+pad_block_size <- function(gr, bin_size, block_size) {
+  # The calculation has several requirements for bin_size and block_size:
+  #   * The range should be multiples of the bin size
+  #   * The block size should also be multiples of the bin size
+  #   * The block size should be smaller or equal than the range
+  #
+  # If these requirements are not met, we will try to make paddings: the bin
+  # size will always stay the same, but the range and the block size will be
+  # adjusted accordingly
+  
+  w <- width(gr)
+  s <- start(gr)
+  e <- end(gr)
+  
+  w2 <- ceiling(w / bin_size) * bin_size
+  s2 <- s
+  e2 <- s2 + w2 - 1
+  block_size <- ceiling(block_size / bin_size) * bin_size
+  
+  stopifnot(block_size <= w2)
+  
+  gr2 <- GRanges(gr)
+  end(gr2) <- e2
+  
+  list(grange = gr2, block_size = block_size)
 }
 
 calc_distance <-
@@ -285,12 +320,11 @@ calc_distance <-
            block_size,
            nthreads = 1,
            metrics = "ks",
-           opts = NULL
-           ) {
+           opts = NULL) {
     # Calculate the distance matrix
     # Parameters:
     # * bam_file: must be indexed. Alignd to a single chromosome.
-    # * gr: GRanges object stating the genomic region of interest
+    # * gr: a string stating the genomic region of interest, e.g. chr22:1-200
     # * bin_zize
     # * block_size: the calculation is performed based on "blocks". Notice that
     #     block_size should be multiples of bin_size
@@ -308,6 +342,22 @@ calc_distance <-
     # Argument check
     # Both the gr range and block_size should be multiples of bin_size
     gr <- GRanges(gr)
+    padding <- pad_block_size(gr, bin_size, block_size)
+    if (!identical(gr, padding$grange) ||
+        !identical(block_size, padding$block_size)) {
+      gr <- padding$gr
+      block_size <- padding$block_size
+      loginfo(
+        sprintf(
+          "Padding: range: %s:%s-%s, block_size: %s",
+          seqnames(gr),
+          format(start(gr), big.mark = ","),
+          format(end(gr), big.mark = ","),
+          format(block_size, big.mark = ",")
+        )
+      )
+    }
+    
     stopifnot(width(gr) %% bin_size == 0)
     stopifnot(block_size %% bin_size == 0)
     stopifnot(block_size <= width(gr))
@@ -317,16 +367,16 @@ calc_distance <-
     
     loginfo(
       sprintf(
-        "Calculation summary: BAM: %s Range: %s:%d-%d, bin_size=%d, block_size=%d, nthreads=%d, metrics=%s, min_samples=%d",
+        "Calculation summary: BAM: %s Range: %s:%s-%s, bin_size=%s, block_size=%s, nthreads=%d, metrics=%s, min_samples=%s",
         bam_file,
         seqnames(gr),
-        start(gr),
-        end(gr),
-        bin_size,
-        block_size,
+        format(start(gr), big.mark = ","),
+        format(end(gr), big.mark = ","),
+        format(bin_size, big.mark = ","),
+        format(block_size, big.mark = ","),
         nthreads,
         metrics,
-        opts$min_samples
+        format(opts$min_samples, big.mark = ",")
       )
     )
     
