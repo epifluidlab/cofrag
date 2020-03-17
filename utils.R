4a3ca8d898331988f0936fc377a50c8a1e13480b
Haizi Zheng <haizi.zh@gmail.com>
2020-03-17 10:43:32 -0400

    New feature:
    * Conver matrices to "short formt" files for Juicer Tools/Pre .hic
      files generation

--- utils.R
+++ utils.R
@@ -4,6 +4,7 @@ default_tick_func <- function(ticks)
   sapply(round(ticks / 1000), function(v)
     sprintf("%s kb", format(v, big.mark = ",")))
 
+# Plot the matrix as a 2D image
 plot_hic_matrix <-
   function(m,
            start,
@@ -29,9 +30,9 @@ plot_hic_matrix <-
       cp <- colorpanel(64, "#FB040A", "#FF7F82", "white")
     else
       cp <- color
-
-    min_val <- min(as.vector(dm[!is.na(dm)]))
-    max_val <- max(as.vector(dm[!is.na(dm)]))
+    
+    min_val <- min(as.vector(m[!is.na(m)]))
+    max_val <- max(as.vector(m[!is.na(m)]))
     zlim <- c(min_val, max_val)
     my.image(
       m[, nrow(m):1],
@@ -67,3 +68,70 @@ plot_hic_matrix <-
       )
     title(main_title, line = 2.5)
   }
+
+# Convert the matrix to "short format" which is required by Pre for .hic generation
+# https://github.com/aidenlab/juicer/wiki/Pre#short-format
+matrix_to_short <- function(data, output_file, nthread = 1) {
+  # Parameters:
+  # * data: a square symmetric matrix. The row names and column names should be an identical
+  #     vector, each element specify the corresponding genomic coordinate.
+  # * output_file: name of the output file.
+  
+  # Argument check
+  stopifnot(nrow(data) == ncol(data))
+  rnames <- rownames(data)
+  cnames <- colnames(data)
+  stopifnot(identical(rnames, cnames))
+  stopifnot(!is.null(rnames))
+  stopifnot(!is.null(output_file))
+  
+  library(stringr)
+  
+  gcoord_split <- function(s) {
+    r <- str_match(s, "([^:]+):(\\d+)")
+    chr <- r[2]
+    pos <- r[3]
+    stopifnot(!is.na(chr))
+    stopifnot(!is.na(pos))
+    c(chr, pos)
+  }
+  
+  # Construct the tasks assignment
+  library(dplyr)
+  # Only process upper triangle entries
+  tasks <- as.matrix(expand.grid(1:nrow(data), 1:ncol(data)) %>% filter(Var1 <= Var2))
+  
+  library(foreach)
+  library(doParallel)
+  
+  cl <- makeCluster(nthread)
+  registerDoParallel(cl)
+  
+  tasks_range <- round(seq(from = 1, to = nrow(tasks) + 1, length.out = nthread + 1))
+  output_lines <- foreach(task_id = 1:nthread, .combine = "c", .export = "str_match") %dopar% {
+    task_start <- tasks_range[task_id]
+    task_end <- tasks_range[task_id + 1] - 1
+    lines <- sapply(task_start:task_end, function(idx) {
+      row_idx <- tasks[idx, 1]
+      col_idx <- tasks[idx, 2]
+      
+      if (is.na(data[row_idx, col_idx]))
+        return(NA)
+      
+      row_gcoord <- gcoord_split(rnames[row_idx])
+      col_gcoord <- gcoord_split(cnames[col_idx])
+      sprintf("0 %s %s 0 0 %s %s 1 %f",
+              row_gcoord[1],
+              row_gcoord[2],
+              col_gcoord[1],
+              col_gcoord[2],
+              data[row_idx, col_idx])
+    })
+    lines[!is.na(lines)]
+  }
+  stopCluster(cl)
+  
+  fileConn <- file(output_file)
+  writeLines(output_lines, fileConn)
+  close(fileConn)
+}
